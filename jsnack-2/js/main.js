e6b87f77822ce30eca36927f502f2d11f0f97d04
Giulia Napoli <gnapoli1996@gmail.com>
2020-11-12 16:11:57 +0100

    Adds template literal - Uploads ex 2

--- jsnack-2/js/main.js
+++ jsnack-2/js/main.js
@@ -1,30 +1,49 @@
 // Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
+let name1 = 'mountainbike';
+let name2 = 'graziella';
+let name3 = 'bmx';
+let name4 = 'downhill';
+
+let weight1 = 10;
+let weight2 = 25;
+let weight3 = 8;
+let weight4 = 15;
 
 const bici = [
   {
-    'nome': 'mountainbike',
-    'peso': 10
+    name1,
+    weight1
   },
   {
-    'nome': 'graziella',
-    'peso': 25
+    name2,
+    weight2
   },
   {
-    'nome': 'bmx',
-    'peso': 8
+    name3,
+    weight3
   },
   {
-    'nome': 'downhill',
-    'peso': 15
+    name4,
+    weight4
   }
 ];
 console.log(bici);
 
-let biciLeggera = bici[0];
-for (let i = 1; i < bici.length; i++) {
+const [myFirstBike, mySecondBike, myThirdBike, myFourthbike] = bici;
+console.log(myFirstBike, mySecondBike, myThirdBike, myFourthbike);
 
-  if (bici[i].peso < biciLeggera.peso) {
-    biciLeggera = bici[i];
-  }
-};
-console.log(biciLeggera);
+let lightBike = 'La bici più leggera è: ';
+
+let templateLiteral = `${lightBike} ${myThirdBike}`;
+console.log(templateLiteral);
+
+document.getElementById('result').innerHTML = templateLiteral;
+
+// let biciLeggera = bici[0];
+// for (let i = 1; i < bici.length; i++) {
+//
+//   if (bici[i].peso < biciLeggera.peso) {
+//     biciLeggera = bici[i];
+//   }
+// };
+// console.log(biciLeggera);
